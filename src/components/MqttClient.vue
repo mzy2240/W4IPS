572ab86f43f63a007f436d323ca62884dd00b236
juntan86 <jun.daniel.tan@gmail.com>
2018-08-08 16:51:11 -0700

    1. mqtt module add clientid/auth

--- src/components/MqttClient.vue
+++ src/components/MqttClient.vue
@@ -6,35 +6,92 @@
 
 <script>
 import mqtt from 'mqtt'
+import fingerprint from 'fingerprintjs2'
 
 export default {
     name: 'MqttClient',
     data() {
         return {
-            client: mqtt.connect('ws://127.0.0.1:8083/mqtt'),
-            message: 'Initialized'
+            client: null,
+            message: 'Initialized',
+            clientid: null
         }
     },
     props: {
         topic: {
-            type: String,
+            type: String || Array || Object,
             default: 'data'
+        },
+        address: {
+            type: String,
+            default: '127.0.0.1:8083/mqtt'
+        },
+        qos: {
+            type: Number,
+            default: 0
+        },
+        protocol: {
+            type: String,
+            define: 'ws'
+        },
+        username: {
+            type: String,
+            default: ''
+        },
+        password: {
+            type: String,
+            define: ''
         }
     },
     created() {
-        // this.client = mqtt.connect('ws://127.0.0.1:8083/mqtt')
-        this.client.on('connect', this.onConnect)
-        this.client.on('message', this.onMessage)
+        this.onInitClient()
     },
     methods: {
-        onConnect() {
+        onInitClient() {
+            this.onGetUUID()
+            this.client = mqtt.connect(
+                this.protocol + '://' + this.address,
+                {
+                    clientId: this.clientid,
+                    username: this.username,
+                    password: this.password
+                }
+            )
+            this.client.on('connect', this.onConnect)
+            this.client.on('message', this.onMessage)
+            this.client.on('close', this.onClose)
+            this.client.on('reconnect', this.onReconnect)
+            this.client.on('offline', this.onOffline)
+            this.client.on('error', this.onError)
+            this.client.on('end', this.onEnd)
+        },
+        onGetUUID() {
+            fingerprint().get((result, components) => {
+                this.clientid = result
+            })
+        },
+        onConnect(connack) {
             console.log('onConnect')
-            this.client.subscribe(this.topic)
-            console.log(this.topic)
+            this.client.subscribe(this.topic, { qos: this.qos })
         },
         onMessage(topic, message) {
             //console.log('#' + topic.toString() + '# ' + message.toString())
             this.message = message.toString()
+        },
+        onClose() {
+            console.log('onClose')
+        },
+        onReconnect() {
+            console.log('onReconnect')
+        },
+        onOffline() {
+            console.log('onOffline')
+        },
+        onError(error) {
+            console.log('onError: ' + error)
+        },
+        onEnd() {
+            console.log('onEnd')
         }
     }
 }
