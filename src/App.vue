f031ab7673484061391a152ea4027d9325293dc6
juntan86 <jun.daniel.tan@gmail.com>
2018-08-07 18:33:40 -0700

    1. Layout ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
+// App.vue
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <v-navigation-drawer
+              fixed
+              :clipped="$vuetify.breakpoint.mdAndUp"
+              app
+              v-model="drawer"
+      >
+          <v-toolbar flat class="transparent">
+              <v-list class="pa-0">
+                  <v-list-tile avatar>
+                      <v-list-tile-avatar>
+                          <v-icon>mdi-xbox-controller</v-icon>
+                          <!--<img src="https://randomuser.me/api/portraits/men/85.jpg" />-->
+                      </v-list-tile-avatar>
+                      <v-list-tile-content>
+                          <v-list-tile-title>Configure</v-list-tile-title>
+                      </v-list-tile-content>
+                  </v-list-tile>
+              </v-list>
+          </v-toolbar>
+          <v-list class="pt-0" dense>
+              <v-divider></v-divider>
+              <v-list-tile>
+                  <v-list-tile-action>
+                      <v-icon></v-icon>
+                  </v-list-tile-action>
+              </v-list-tile>
+          </v-list>
+      </v-navigation-drawer>
+    <v-toolbar color="blue darken-3"
+               dark
+               app
+               :clipped-left="$vuetify.breakpoint.mdAndUp"
+               fixed>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title class="white--text">PowerWeb</v-toolbar-title>
+        <v-toolbar-items>
+        </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      drawer: false
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import './registerServiceWorker'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.config.productionTip = false;
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+  components: { App },
+  template: '<App/>'
+}).$mount('#app')
