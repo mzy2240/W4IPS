55b11076179cd649498a6f759f937b55fc945e31
Mao <zeyumao2@tamu.edu>
2018-08-30 17:06:06 -0500

    1. change the mqtt server address

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-    <!-- <HelloWorld></HelloWorld> -->
-    <directchart></directchart>
-    <!-- <mapchart></mapchart> -->
-      <!-- <MqttClient  style="font-size: 12px"></MqttClient> -->
-    <!-- <ApiClient></ApiClient> -->
-  </div>
+	<div>
+		<!-- <HelloWorld></HelloWorld> -->
+		<directchart></directchart>
+		<!-- <mapchart></mapchart> -->
+		<MqttClient style="font-size: 12px"></MqttClient>
+		<!-- <ApiClient></ApiClient> -->
+	</div>
 </template>
 
 <style>
 .chart {
-  height: 800px;
-  width: 800px;
+	height: 800px;
+	width: 800px;
 }
 </style>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld'
-import MqttClient from '@/components/MqttClient'
-import ApiClient from '@/components/ApiClient'
-import mapchart from '@/components/map'
-import directchart from '@/components/puremap'
+import HelloWorld from '@/components/HelloWorld';
+import MqttClient from '@/components/MqttClient';
+import ApiClient from '@/components/ApiClient';
+import mapchart from '@/components/map';
+import directchart from '@/components/puremap';
 
 export default {
-    name: 'home',
-    components: {
-        HelloWorld,
-        MqttClient,
-        ApiClient,
-        mapchart,
-        directchart
-    }
-}
+	name: 'home',
+	components: {
+		HelloWorld,
+		MqttClient,
+		ApiClient,
+		mapchart,
+		directchart
+	}
+};
 </script>
