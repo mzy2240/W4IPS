5144f5b1ba02b71ce1ee3592874fb6d3119291eb
Mao <zeyumao2@tamu.edu>
2018-09-07 16:36:24 -0500

    1. recovery

--- src/store.js
+++ src/store.js
@@ -1,39 +1,52 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import datafield from './assets/datafield.json'
+import tcmcommands from './assets/tcmcommands.json'
+import casedetail from './assets/150.json'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    toPublish: 0,
-    message: [],
-    UUID: ''
-  },
-  getters: {
-    getPubStatus(state) {
-      return state.toPublish
-    },
-    getMessage(state) {
-      return state.message
-    }
-  },
-  mutations: {
-    setPublish (state) {
-      state.toPublish++
-    },
-    reset(state) {
-      state.toPublish = 0
-    },
-    setMessage (state, payload) {
-      state.message = payload
-    },
-    setUUID(state, payload) {
-      state.UUID = payload
-    }
-  },
-  actions: {
-    updatemessage({commit}, payload) {
-      commit('setMessage', payload)
-    }
-  }
+	state: {
+		toPublish: 0,
+		message: '',
+		UUID: '',
+		fieldstore: datafield,
+		tcmcommands: tcmcommands,
+		casedetail: casedetail,
+		rawdata: {}
+	},
+	getters: {
+		getPubStatus(state) {
+			return state.toPublish;
+		},
+		getMessage(state) {
+			return state.message;
+		}
+	},
+	mutations: {
+		setPublish(state) {
+			state.toPublish++;
+		},
+		reset(state) {
+			state.toPublish = 0;
+		},
+		setMessage(state, payload) {
+			state.message = payload;
+		},
+		setUUID(state, payload) {
+			state.UUID = payload;
+		},
+		updateConfig(state, payload){
+			state.casedetail = payload;
+		},
+		updateRawData(state, payload) {
+			state.rawdata = payload;
+		}
+	},
+	actions: {
+		updatemessage({ commit }, payload) {
+			commit('setMessage', payload);
+		}
+	}
 });
