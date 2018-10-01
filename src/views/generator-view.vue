4fb9ac7c277c7dc2230e74ecc7f720b0a6b09283
Mao <zeyumao2@tamu.edu>
2018-10-01 16:47:49 -0500

    1. Add a table template to the generator page

--- src/views/generator-view.vue
+++ src/views/generator-view.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is a generator page</h1>
-  </div>
+	<v-container grid-list-xs text-xs-center>
+		<v-layout row wrap>
+			<v-flex d-flex xs6 style="height: auto;">
+				<gentable></gentable>
+			</v-flex>
+			<v-flex d-flex xs6 style="height: 700px;">
+				<v-layout row wrap>
+					<v-flex style="height:50%;" d-flex xs12>
+						<v-card dark color="light-blue">
+							<v-card-text class="px-0">Tool1</v-card-text>
+						</v-card>
+					</v-flex>
+					<v-flex style="height:50%;" d-flex xs12>
+						<v-card dark color="light-blue">
+							<v-card-text class="px-0">Tool2</v-card-text>
+						</v-card>
+					</v-flex>
+				</v-layout>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
+
+<style>
+.container {
+	max-width: 100vw;
+	padding: 0px;
+	margin: 5px;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld';
+// import ApiClient from '@/components/ApiClient';
+// import mapchart from '@/components/map';
+import gentable from '@/components/GenTable';
+
+export default {
+	name: 'generator',
+	components: {
+		// HelloWorld,
+		// ApiClient,
+		// mapchart,
+		gentable
+	}
+};
+</script>
